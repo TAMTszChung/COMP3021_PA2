d3ddcffd341911dc6d5ba417cccaf8f31671efde
TAMTszChung <tctam@connect.ust.hk>
2020-11-08 22:30:49 +0800

    Update FXJesonMor

--- src/main/java/castle/comp3021/assignment/gui/FXJesonMor.java
+++ src/main/java/castle/comp3021/assignment/gui/FXJesonMor.java
@@ -31,6 +31,12 @@ public class FXJesonMor extends JesonMor {
     public FXJesonMor(Configuration configuration){
         //TODO
         super(configuration);
+        Piece[][] oldBoard = configuration.getInitialBoard();
+        configuration.setAllInitialPieces();
+        for (Player player : configuration.getPlayers()) {
+            player.setScore(0);
+        }
+
         this.durationTimer = new DurationTimer();
     }
 
